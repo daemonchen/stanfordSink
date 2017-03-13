60cba70609845cffd7bd6d6b071f46f3b780cd7f
悠歌 <guangyuan.cgy@alibaba-inc.com>
2017-03-13 23:45:31 +0800

    Initial Commit

---
+++ myIosTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myIosTest
+//
+//  Created by scott on 2017/3/13.
+//  Copyright © 2017年 scott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myIosTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myIosTest
+//
+//  Created by scott on 2017/3/13.
+//  Copyright © 2017年 scott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
